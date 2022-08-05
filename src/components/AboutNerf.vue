f234adb13b5850ceb3a82d11da3ddf967744682a
Jiawei-Zhao-728 <zhaoranjiawei@gmail.com>
2022-08-04 22:13:44 -0400

    added info

--- src/components/AboutNerf.vue
+++ src/components/AboutNerf.vue
@@ -32,7 +32,43 @@ import SupportIcon from "./icons/IconSupport.vue";
     <template #icon>
       <ToolingIcon />
     </template>
-    <template #heading>How do I use this website?</template>
-    IDK dude
+    <template #heading
+      >Front-end Structure and Framwork -- Vue.js with i18n plugin</template
+    >
+    For the website portion of this project, we chose to use the java script
+    framework called Vue to host all the components for the website. Compare to
+    other popular frameworks, Vue is a lot more popular, and relatively easy to
+    get started for beginners, also, it support a wide range of javascript
+    libraries that could be useful for the project. The i18n plugin as mentioned
+    is to let the website have multi-language support, as that can let more
+    users around the world have a taste of what Nerf technology is all about.
+    <br />
+    <br />
+    The purpose of the Frontend is to create a user interface that first can
+    display information about the technology, more importantly, it servers as an
+    input source for users to input the videos that he/she wants to create 3d
+    object for. Therefore, it’s crucial for the website to have the ability to
+    take videos from user input, and send it to the backend for processing via
+    the web server.
+  </ListItem>
+
+  <ListItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading
+      >Back-end Structure and Framwork -- Neural Radiance Field Technology via
+      Colmap.
+    </template>
+  </ListItem>
+
+  <ListItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading
+      >Webserver Structure and Framwork -- Flask with Mongdb and
+      Rabbitmq</template
+    >
   </ListItem>
 </template>
