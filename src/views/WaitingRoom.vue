f4dc8f02358056574a8c583cdcf9bb5674abb21e
delexagon <delexagon@gmail.com>
2022-08-09 17:51:58 -0400

    Added text to show video status

--- src/views/WaitingRoom.vue
+++ src/views/WaitingRoom.vue
@@ -11,9 +11,11 @@ const route = useRoute();
 const video = route.query.video;
 
 const vidurl = ref("");
+const vidstatus = ref("Not done");
+const showvid = ref(false);
 
 function getStatus() {
-  let url = "http://localhost:5000/video/" + video
+  let url = "http://localhost:5000/nerfvideo/" + video
   fetch(url, {
     method: 'GET',
     cache: 'no-cache'
@@ -22,28 +24,40 @@ function getStatus() {
     function(response) {
       response.blob().then(
         function(blob) {
-          vidurl.value = URL.createObjectURL(blob);
+          if(blob.type === 'video/mp4') {
+            vidurl.value = URL.createObjectURL(blob);
+            vidstatus.value = "Ready";
+            showvid.value = true;
+            clearInterval(intervalID);
+          } else {
+            blob.text().then(
+              function(text) {
+                vidstatus.value = text;
+              }
+            )
+          }
         }
       )
     }
   )
   .catch(
     function(error) {
-      console.log(error)
+        vidstatus.value = "Connection error"
     }
   )
 }
 
-getStatus()
-const intervalID = setInterval(getStatus, 30000)
+getStatus();
+const intervalID = setInterval(getStatus, 30000);
 
-onUnmounted(() => clearInterval(intervalID))
+onUnmounted(() => clearInterval(intervalID));
 </script>
 
 <template>
   <div class="about">
     <h1>Welcome to the waiting room, your video is {{ video }}.</h1>
     <p>Copy the url to return to this page and get your video.</p>
-    <p>Video status: <a v-bind:href="vidurl">Download</a></p>
+    <h4>Video status: {{vidstatus}}</h4>
+    <p v-if='showvid'><a v-bind:href="vidurl">Download</a></p>
   </div>
 </template>
