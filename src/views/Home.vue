bff1f6854b9b8c025920b7b2cc41c715ffbef13b
“jxlyn” <jlzou0604@gmail.com>
2022-08-15 01:36:15 -0400

    finish homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -26,14 +26,43 @@
    Abstract & Method:
 </h1>
 <h2>
-   &nbsp;&nbsp;&nbsp;&nbsp; We present a model that can convert the 
+   &nbsp;&nbsp;&nbsp;&nbsp; We present a model that reconstruct the radiance field and is able to generate novel views of a complex
+   3D scenes based on a set of 2D images. We will use COLMAP to reconstruct the object. The COLMAP consists of intrinsic matrix of all 
+   reconstructed cameras, pose and key points of all reconstructed images. The reconstructed pose of an image is specified as the projection 
+   from world to the camera coordinate system. The data conversion consists of a quaternion (QW, QX, QY, QZ) and a translation vector (TX, TY, TZ). 
+   Our model will scale output to Normalized device coordinate (NDC Space).
+
 </h2>
 
+<img src="../assets/model.png" width="560" height="345" >
+<img src="../assets/formula.png" width="650" height="345" >
+ 
+
+<h1>
+   Expected outcomes:
+</h1>
 
 
+<video width="560" height="345" controls>
+  <source src="../assets/lego_demo.mp4" type="video/mp4">
+</video>
 
-
-
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<video width="560" height="345" controls>
+  <source src="../assets/flower_demo.mp4" type="video/mp4">
+</video>
 
   </div>
 </template>
+
+
+
+
+
+
+
+
+
+
+
+
