c549fa763459c2a637ae6ae472cb19657c139662
GreenMatter <white00hart@gmail.com>
2022-08-17 14:07:12 -0400

    adjust some effects

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -9,13 +9,15 @@ function setFile() {
   var curfile = document.getElementById("input").files[0];
   file.value = curfile;
   filename.value = curfile.name;
+
 }
 </script>
 
 <template>
+  <link href="https://fonts.googleapis.com/css?family=Work+Sans:400,600,800&display=swap" rel="stylesheet">
   <div class="parent">
     <input id="input" type="file" @input='setFile()'>
-    <p>{{filename}}</p>
+    <p ref="ff" class="rainbow-text">{{filename}}</p>
     <!--      <button type="submit" @click='$emit("sendFile", file)'>-->
     <div style="width: 100%;text-align: center">
       <svg @click='$emit("sendFile", file)' style="margin: auto;" width="100" height="100" viewBox="0 0 100 100">
@@ -218,6 +220,87 @@ button{
     stroke-dasharray: 31 82;
     stroke-dashoffset: -50px;
     transform: translateY(-65px);
+  }
+}
+
+
+/* General styling of the page. */
+/* Sets a background color, font-size etc. */
+body {
+  background-color: #EDDDD4;
+}
+
+
+
+/* Style the rainbow text element. */
+.rainbow-text {
+  /* Create a conic gradient. */
+  /* Double percentages to avoid blur (#000 10%, #fff 10%, #fff 20%, ...). */
+  background: #CA4246;
+  background: conic-gradient(
+    #CA4246 16.666%,
+    #E16541 16.666%,
+    #E16541 33.333%,
+    #F18F43 33.333%,
+    #F18F43 50%,
+    #8B9862 50%,
+    #8B9862 66.666%,
+    #476098 66.666%,
+    #476098 83.333%,
+    #A7489B 83.333%);
+
+  /* Set thee background size and repeat properties. */
+  background-size: 57%;
+  background-repeat: repeat;
+
+  /* Use the text as a mask for the background. */
+  /* This will show the gradient as a text color rather than element bg. */
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+  /* Animate the text when loading the element. */
+  /* This animates it on page load and when hovering out. */
+  animation: rainbow-text-animation-rev 0.5s ease forwards;
+
+  cursor: pointer;
+}
+
+/* Add animation on hover. */
+.rainbow-text:hover {
+  animation: rainbow-text-animation 0.5s ease forwards;
+}
+
+/* Move the background and make it larger. */
+/* Animation shown when hovering over the text. */
+@keyframes rainbow-text-animation {
+  0% {
+    background-size: 57%;
+    background-position: 0 0;
+  }
+  20% {
+    background-size: 57%;
+    background-position: 0 1em;
+  }
+  100% {
+    background-size: 300%;
+    background-position: -9em 1em;
+  }
+}
+
+/* Move the background and make it smaller. */
+/* Animation shown when entering the page and after the hover animation. */
+@keyframes rainbow-text-animation-rev {
+  0% {
+    background-size: 300%;
+    background-position: -9em 1em;
+  }
+  20% {
+    background-size: 57%;
+    background-position: 0 1em;
+  }
+  100% {
+    background-size: 57%;
+    background-position: 0 0;
   }
 }
 
